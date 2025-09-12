fff371e45a1761acc9b27053a982d27f9b9ddd11
1rv1nn <1rv1n@ciencias.unam.mx>
2025-09-12 00:48:48 -0600

    feat: supervisor support implemented

--- blog-resenas-materias/ecosystem.config.js
+++ blog-resenas-materias/ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [{
     name: "blog-resenas-materias",
-    script: "serve",
-    args: "-s dist -l 3000",
-    exec_mode: "cluster",
-    instances: "max",
+    script: "sh",
+    args: "-c 'serve -s /app/dist -l 3000'",
+    exec_mode: "fork",
+    instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: "1G",
---
+++ blog-resenas-materias/start-pm2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Iniciar el servidor directamente
+exec serve -s /app/dist -l 3000
